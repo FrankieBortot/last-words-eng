9e09828ded3c335ac3d73430cd150eef8b2f1b7a
FrankieBortot <bortot.frankie@gmail.com>
2021-07-10 22:21:33 +0200

    update

---
+++ script0.js
@@ -0,0 +1,23 @@
+function reveal2() {
+    document.getElementById("part2").style.display = "block";
+}
+
+function revea3() {
+    document.getElementById("part3").style.display = "block";
+}
+
+function reveal4() {
+    document.getElementById("part4").style.display = "block";
+}
+
+function revea5() {
+    document.getElementById("part5").style.display = "block";
+}
+
+function revea6() {
+    document.getElementById("part6").style.display = "block";
+}
+
+function revea7() {
+    document.getElementById("part7").style.display = "block";
+}
